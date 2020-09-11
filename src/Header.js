c2577f3cb02e22cd53aa6969232706898b08d64f
Tali Ngatuvai <Tngatuvai85@gmail.com>
2020-09-11 16:09:02 -0600

    The set-up & the HEADER

---
+++ src/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+    return (
+        <div className='header'>
+            <img className="header_logo" 
+            src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+        
+
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            <div className="header_nav">
+                <div className="header_option">
+                    <span className="header_optionLineOne">Hello Guess</span>
+                    <span className="header_optionLineTwo">Sign In</span>
+                </div>
+                
+                <div className="header_option">
+                    <span className="header_optionLineOne">Returns</span>
+                    <span className="header_optionLineTwo">& Orders</span>
+
+                </div>
+                
+                <div className="header_option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineTwo">Prime</span>
+                </div>
+
+                <div className="header_option">
+                    <span className="header_optionLineOne">Hello Guess</span>
+                    <span className="header_optionLineTwo">Sign In</span>
+
+                </div>
+
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header_basketCount">0</span>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
