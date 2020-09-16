c904415abc68451cae5faf866645e5d9028ccfdc
Tali Ngatuvai <Tngatuvai85@gmail.com>
2020-09-16 00:20:51 -0600

    Got the props rendering the product details

--- src/Home.js
+++ src/Home.js
@@ -13,24 +13,54 @@ function Home() {
         />
 
         <div className="home__row">
-            <Product />
-            <Product />
-            
-
+          <Product
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="1234"
+            title="Kenwood MixStand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
+            price={499.0}
+            rating={3}
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
+          />
         </div>
 
         <div className="home__row">
-            <Product />
-            <Product />
-            <Product />
-                
+          <Product
+            id="12345"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+            price={699.99}
+            rating={2}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="123456"
+            title="Amazon Echo (4th generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={79.99}
+            rating={4.5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="1234567"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={999.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+          />
         </div>
 
         <div className="home__row">
-            <Product />
-            
+          <Product 
+            id="12345678"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1299.99}
+            rating={3.5}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
         </div>
-
       </div>
     </div>
   );
